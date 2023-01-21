95acf2268ddb22a318dc6e24c5e8ddd57a4e6b8f
Kernel Srinivas <kernel.srinivasan@gmail.com>
2023-01-21 20:33:54 +0530

    home page and mock json update

--- src/app-route/AppRoute.index.tsx
+++ src/app-route/AppRoute.index.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import DetailPage from '../detail-page/DetailPage.index';
+import HomePage from '../home-page/HomePage.index';
+import ListPage from '../list-page/ListPage.index';
+import LoginPage from '../login-page/LoginPage.index';
+import PaymentPage from '../payment-page/Payment.index';
+
+const Stack = createStackNavigator();
 
 const AppRoute = (): JSX.Element => {
   return (
     <View>
-      <Text>AppRoute</Text>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomePage} />
+          <Stack.Screen name="Details" component={DetailPage} />
+          <Stack.Screen name="List" component={ListPage} />
+          <Stack.Screen name="Login" component={LoginPage} />
+          <Stack.Screen name="Payment" component={PaymentPage} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
   );
 };
